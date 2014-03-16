5cec2f19aa0e84cf9b47ce96a80304400a7320d9
Yasuhiro Inami <inamiy@gmail.com>
2014-03-16 13:05:38 +0900

    Add 'Stealing popGesture-delegate' example at tabIndex=2 (similar to Facebook 7.0 app, better interaction compared to usesStrictMode=YES)

--- Demo/YIStrictEdgePanGestureDemo/AppDelegate.m
+++ Demo/YIStrictEdgePanGestureDemo/AppDelegate.m
@@ -20,9 +20,20 @@
             UINavigationController* navC = tabC.viewControllers[i];
             [navC.topViewController performSegueWithIdentifier:@"PushSegue" sender:self];
             
+            UIScreenEdgePanGestureRecognizer* popGesture = (id)navC.interactivePopGestureRecognizer;
+            
             if (i == 1) {
-                UIScreenEdgePanGestureRecognizer* popGesture = (id)navC.interactivePopGestureRecognizer;
                 popGesture.usesStrictMode = NO;
+            }
+            else if (i == 2) {
+                popGesture.usesStrictMode = NO;
+                
+                //
+                // DEMO:
+                // Steal popGesture.delegate from _UINavigationInteractiveTransition
+                // not to perform popGesture while content-scrollView is active (like Facebook 7.0 app).
+                //
+                navC.interactivePopGestureRecognizer.delegate = (id)self;
             }
         }
     });
@@ -30,32 +41,30 @@
     // Override point for customization after application launch.
     return YES;
 }
-							
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    UITabBarController* tabC = (id)self.window.rootViewController;
+    UINavigationController* navC = (id)tabC.selectedViewController;
+    
+    if (gestureRecognizer == navC.interactivePopGestureRecognizer) {
+        
+        UITableViewController* tableVC = (id)navC.topViewController;
+        
+        // don't perform popGesture while dragging
+        if (tableVC.tableView.dragging) return NO;
+        
+        //
+        // NOTE:
+        // NEVER perform popGesture inside navigationController's rootViewController, or animation-lock occurs
+        // (probably same as _UINavigationInteractiveTransition's implementation)
+        //
+        // http://stackoverflow.com/questions/19054625/changing-back-button-in-ios-7-disables-swipe-to-navigate-back/20330647#comment28683247_19133676
+        //
+        return [navC.viewControllers count] > 1;
+        
+    }
+    return YES;
 }
 
 @end
